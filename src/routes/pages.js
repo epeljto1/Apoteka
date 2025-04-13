749f836c07f3fe51320356447b1d01710365bca8
HBosno <hbosno1@etf.unsa.ba>
2025-04-13 15:35:10 +0200

    minor fix

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -8,7 +8,7 @@ router.get('/login', (req, res) => {
 
 router.get('/products', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/html/products.html'));
-  
+});
 router.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/html/dashboard.html'));
 });
